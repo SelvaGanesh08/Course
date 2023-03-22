dd5f9ca6a021a6ccda4e9150acc2d7a171cce5c8
SelvaGanesh08 <selvaganesh0809@gmail.com>
2023-03-22 08:56:55 +0530

    Correcting NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -12,37 +12,40 @@ function NavBar() {
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-3 col-md-3">
-                          <Link to ='/home' className='link-logo'>
-                          <div className="site-logo">
-                                <img src={logo} alt="" />
-                                <h1>CourseCuer</h1>
+                            <Link to='/home' className='link-logo'>
+                                <div className="site-logo">
+                                    <img src={logo} alt="" />
+                                    <h1>CourseCuer</h1>
                                 </div>
-                                </Link>
+                            </Link>
                             <div className="nav-switch">
                                 <i className="fa fa-bars" />
                             </div>
                         </div>
                         <div className="col-lg-9 col-md-9">
-                            <a href="" className="site-btn header-btn">
-                                Login
-                            </a>
+                            <Link to='/login' className='link-logo'>
+                                <h6 href="" className="site-btn header-btn">
+                                    Login
+                                </h6>
+                            </Link>
+
                             <nav className="main-menu">
                                 <ul>
+
                                     <li>
-                                        <Link to ='/home' className='main-menu-item'>Home</Link>
+                                        <Link to='/search' className='main-menu-item'>Favourites</Link>
                                     </li>
                                     <li>
-                                    <Link to ='/search' className='main-menu-item'>Search</Link>
-                                    </li>
-                                    {/* <li>
-                                        <a href="courses.html">Courses</a>
+                                        <Link to='/search' className='main-menu-item'>Profile</Link>
                                     </li>
                                     <li>
-                                        <a href="blog.html">News</a>
+                                        <Link to='/search' className='main-menu-item'>Search</Link>
                                     </li>
                                     <li>
-                                        <a href="contact.html">Contact</a>
-                                    </li> */}
+                                        <Link to='/home' className='main-menu-item'>Home</Link>
+                                    </li>
+                                    
+
                                 </ul>
                             </nav>
                         </div>
