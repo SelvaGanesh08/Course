74c699ae5e111960dbf0628224a383e42f6ea05e
SelvaGanesh08 <selvaganesh0809@gmail.com>
2023-03-24 22:59:23 +0530

    Backend code

--- backend/server/api/models.py
+++ backend/server/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    rating = models.FloatField()
+    reviews = models.IntegerField()
+
+    class Meta:
+        db_table = 'courses'
+
+    def __str__(self):
+        return self.title
+
--- backend/server/api/views.py
+++ backend/server/api/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Course
 
-# Create your views here.
+import csv
+from django.shortcuts import render
+from .models import Course
+
+def load_courses(request):
+    with open('path/to/courses.csv') as f:
+        reader = csv.reader(f)
+        next(reader) # Skip the header row
+        for row in reader:
+            title = row[0]
+            rating = float(row[1])
+            reviews = int(row[2])
+            course = Course(title=title, rating=rating, reviews=reviews)
+            course.save()
+
+    return render(request, 'courses_loaded.html')
+
+
+
+
+def recommend_courses(request, title):
+    # Step 1: Get the input course by title
+    try:
+        input_course = Course.objects.get(title=title)
+    except Course.DoesNotExist:
+        return JsonResponse({'error': 'Course not found.'})
+
+    # Step 2: Calculate the similarity scores
+    ratings = Course.objects.values_list('rating', 'reviews')
+    similarity_matrix = cosine_similarity(ratings)
+
+    # Step 3: Get the indices of the courses similar to the input course
+    input_index = input_course.pk - 1
+    sim_scores = list(enumerate(similarity_matrix[input_index]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:6] # Get the top 5 similar courses
+
+    # Step 4: Get the recommended courses
+    course_indices = [x[0] for x in sim_scores]
+    recommended_courses = Course.objects.filter(pk__in=course_indices)
+
+    # Step 5: Return the recommended courses
+    data = {
+        'input_course': input_course.title,
+        'recommended_courses': [c.title for c in recommended_courses]
+    }
+    return JsonResponse(data)
+
