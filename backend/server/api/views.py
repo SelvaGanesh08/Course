b810ecc47a129ea01d8b33b4939b9b6230db5745
SelvaGanesh08 <selvaganesh0809@gmail.com>
2023-03-31 00:45:55 +0530

    Backend

--- backend/server/api/views.py
+++ backend/server/api/views.py
@@ -40,7 +40,7 @@ class LoginView(APIView):
 
 
 def udemy_load_courses(request):
-    with open(r'C:\Users\Students\Desktop\Course\backend\server\api\data\Course_info.csv','r',encoding='latin-1') as f:
+    with open(r'C:\Users\Ganork\Desktop\Course\backend\server\api\data\Course_info.csv','r',encoding='latin-1') as f:
         reader = csv.reader(f)
         next(reader) 
         for row in reader:
@@ -54,7 +54,7 @@ def udemy_load_courses(request):
     return HttpResponse(row)
 
 def cousera_load_courses(request):
-    with open(r'C:\Users\Students\Desktop\Course\backend\server\api\data\coursera1.csv','r',encoding='latin-1') as f:
+    with open(r'C:\Users\Ganork\Desktop\Course\backend\server\api\data\coursera1.csv','r',encoding='latin-1') as f:
         reader = csv.reader(f)
         next(reader) 
         for row in reader:
